dce1b31b9a53b3fa0570f6322850a1d27a16591b
Stephen Iskander <siskande@oswego.edu>
2018-05-13 11:04:47 -0400

    GUI and working gradle build

--- src/main/java/edu/oswego/raftchat/App.java
+++ src/main/java/edu/oswego/raftchat/App.java
@@ -2,7 +2,16 @@ package edu.oswego.raftchat;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
+import edu.oswego.raftchat.components.ConnectDialog;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.net.InetSocketAddress;
@@ -12,8 +21,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class App extends Application {
-    static InetSocketAddress initAddress;
-    BlockingQueue<ChatMessage> chatQueue = new LinkedBlockingQueue<>();
+    private static Options options;
+    private static InetSocketAddress initAddress;
+    static BlockingQueue<ChatMessage> chatQueue = new LinkedBlockingQueue<>();
+
+    private String username;
+    private TextField chatField;
+    private TextArea chatArea;
 
     private static class Options {
         @Parameter(names = "-no-client", description = "Run raft peer without chat client")
@@ -30,18 +44,20 @@ public class App extends Application {
     }
 
     public static void main(String[] args) {
-        Options options = new Options();
+        options = new Options();
         JCommander.newBuilder()
                 .addObject(options)
                 .build()
                 .parse(args);
 
         if (options.server != null) {
-            Matcher addressMatcher = Pattern.compile("(.*?):(\\d+)").matcher(options.server);
-            String host = addressMatcher.group(1);
-            int port = Integer.parseInt(addressMatcher.group(2));
+            Matcher addressMatcher = Pattern.compile("^(.*):(\\d+)$").matcher(options.server.trim());
+            if (addressMatcher.matches()) {
+                String host = addressMatcher.group(1);
+                int port = Integer.parseInt(addressMatcher.group(2));
 
-            initAddress = new InetSocketAddress(host, port);
+                initAddress = new InetSocketAddress(host, port);
+            }
         }
 
         if (options.noClient) {
@@ -53,6 +69,34 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
+        ConnectDialog connectDialog;
 
+        if(initAddress != null) {
+            connectDialog = new ConnectDialog(initAddress.getHostName(), initAddress.getPort());
+        } else {
+            connectDialog = new ConnectDialog();
+        }
+
+        new ConnectDialog().showAndWait().ifPresent(m -> {
+            username = m.get("username");
+            String addr = m.get("server");
+
+            int port = Integer.parseInt(m.get("port"));
+
+            chatField = new TextField();
+            chatArea = new TextArea();
+            VBox.setVgrow(chatArea, Priority.ALWAYS);
+            chatArea.setEditable(false);
+            HBox.setHgrow(chatField, Priority.ALWAYS);
+            Button sendButton = new Button("Send");
+            HBox inputBox = new HBox(10, chatField, sendButton);
+
+
+            VBox root = new VBox(10, chatArea, inputBox);
+            root.setPadding(new Insets(10, 10, 10, 10));
+            stage.setScene(new Scene(root));
+            chatField.requestFocus();
+            stage.show();
+        });
     }
 }
---
+++ src/main/java/edu/oswego/raftchat/components/ConnectDialog.java
@@ -0,0 +1,70 @@
+package edu.oswego.raftchat.components;
+
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ConnectDialog extends Dialog<Map<String, String>> {
+    private static final ButtonType connectButtonType = new ButtonType("Connect", ButtonBar.ButtonData.OK_DONE);
+
+    public ConnectDialog() {
+        this("pi.cs.oswego.edu", 2706);
+    }
+
+    public ConnectDialog(String serverName, int portNum) {
+        super();
+
+        getDialogPane().getButtonTypes().addAll(connectButtonType, ButtonType.CANCEL);
+        setTitle("Connect to Server");
+
+        VBox mainBox = new VBox();
+        mainBox.setPadding(new Insets(10, 75, 10, 10));
+        mainBox.setSpacing(10);
+
+        HBox serverBox = new HBox();
+        serverBox.setSpacing(10);
+
+        TextField username = new TextField();
+        username.setPromptText("Username");
+        TextField server = new TextField();
+        server.setText(serverName);
+        server.setPromptText("Server");
+        TextField port = new TextField();
+        port.setMinWidth(40);
+        port.setText(Integer.toString(portNum));
+        port.setPromptText("Port");
+        port.prefWidthProperty().bind(serverBox.widthProperty().divide(3));
+
+        serverBox.getChildren().addAll(server, port);
+        mainBox.getChildren().addAll(username, serverBox);
+
+        getDialogPane().setContent(mainBox);
+
+        Node connectButton = getDialogPane().lookupButton(connectButtonType);
+        connectButton.disableProperty().bind(username.textProperty().isEmpty()
+                .or(server.textProperty().isEmpty())
+                .or(port.textProperty().isEmpty()));
+
+        Platform.runLater(username::requestFocus);
+
+        setResultConverter(type -> {
+            if(type == connectButtonType) {
+                Map<String, String> result = new HashMap<>();
+                result.put("username", username.getText().trim());
+                result.put("server", server.getText().trim());
+                result.put("port", port.getText().trim());
+                return result;
+            }
+            return null;
+        });
+    }
+}
